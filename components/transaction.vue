1c14b5635dc705bf60821888bcc903a36bd9f380
Your Name <your.email@example.com>
2024-06-08 10:18:48 -0700

    Chevron only shows if text is truncated

--- components/transaction.vue
+++ components/transaction.vue
@@ -1,14 +1,13 @@
 <template>
-  <div
-    class="border-b border-border-color py-2 text-default-text "
-  >
+  <div class="border-b border-border-color py-2 text-default-text">
     <div class="flex justify-between items-center gap-14">
-      <div class="flex justify-between items-center w-3/4 "  >
-        <div class="flex items-center gap-4 justify-start w-3/4 ">
+      <div class="flex justify-between items-center w-3/4">
+        <div class="flex items-center gap-4 justify-start w-3/4">
           <Icon :name="icon" class="icon text-[24px] }" :class="[iconColor]" />
-          <p class="truncate" :class="{'showing': showMore}">{{ props.transaction.description }}
-            </p>
-            <UIcon :name="chevronIcon"  @click="showMore =!showMore"/>
+          <p class="description-text truncate" ref="descriptionRef"  :class="{ showing: showMore }">
+            {{ props.transaction.description }}
+          </p>
+          <UIcon :name="chevronIcon" @click="showMore = !showMore"  v-if="isOverflowing" />
         </div>
         <UBadge
           v-show="
@@ -19,29 +18,28 @@
         >
       </div>
 
+      <TransactionUpdateModal
+        :transaction="props.transaction"
+        v-model="updateModalIsOpen"
+        @closeModal="updateModalIsOpen = false"
+        @updated="updated"
+      />
 
-    <TransactionUpdateModal
-      :transaction="props.transaction"
-      v-model="updateModalIsOpen"
-      @closeModal="updateModalIsOpen = false"
-      @updated="updated"
-    />
-
-    <div class="flex justify-end space-x-2 items-center w-fit">
-      <div>{{ currency }}</div>
-      <div>
-        <UDropdown :items="items" :popper="{ placement: 'bottom-start' }">
-          <UButton
-            color="white"
-            variant="ghost"
-            trailing-icon="i-heroicons-chevron-down-20-solid"
-            :loading="isLoading"
-          />
-        </UDropdown>
+      <div class="flex justify-end space-x-2 items-center w-fit">
+        <div>{{ currency }}</div>
+        <div>
+          <UDropdown :items="items" :popper="{ placement: 'bottom-start' }">
+            <UButton
+              color="white"
+              variant="ghost"
+              trailing-icon="i-heroicons-chevron-down-20-solid"
+              :loading="isLoading"
+            />
+          </UDropdown>
+        </div>
       </div>
     </div>
   </div>
-  </div>  
 </template>
 
 <script setup>
@@ -49,6 +47,8 @@ const toast = useToast();
 const supabase = useSupabaseClient();
 const updateModalIsOpen = ref(false);
 const showMore = ref(false);
+const descriptionRef = ref(null);
+const isOverflowing = ref(false);
 
 const props = defineProps({
   transaction: Object,
@@ -159,9 +159,11 @@ const iconColor = computed(() => {
   }
 });
 
-const chevronIcon = computed(()=>{
-  return showMore.value ? "i-heroicons-chevron-up-20-solid" : "i-heroicons-chevron-down-20-solid"
-})
+const chevronIcon = computed(() => {
+  return showMore.value
+    ? "i-heroicons-chevron-up-20-solid"
+    : "i-heroicons-chevron-down-20-solid";
+});
 
 const currency = computed(() => {
   return new Intl.NumberFormat("en-In", {
@@ -173,15 +175,24 @@ const currency = computed(() => {
       : props.transaction.amount
   );
 });
+
+onMounted(() => {
+  const element = descriptionRef.value;
+  if (element) {
+    isOverflowing.value = element.scrollWidth > element.clientWidth;
+  }
+});
+
 </script>
 
 <style lang="scss" scoped>
+.truncate {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
 .showing {
   overflow: visible;
-  max-width: 100%;
   white-space: wrap;
-  z-index: 1000;
-  overflow: visible;
-
 }
 </style>
